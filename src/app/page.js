0736c07a7995a501797e76ba2c64fd107f648e05
Almir <allo0001@stud.kea.dk>
2026-04-20 10:06:11 +0200

    Add framer-motion dependency and implement typewriter effect in homepage

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,15 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <Link href="/products">
-        <p>hej</p>
-      </Link>
-    </>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center">
+        <h1 className="text-7xl typewriter">A&D Supply</h1>
+        <Link href="/products">
+          <button className="mt-10 border px-8 py-2 rounded-full animate-fadeIn">
+            VIEW PRODUCTS
+          </button>
+        </Link>
+      </div>
+    </div>
   );
 }
